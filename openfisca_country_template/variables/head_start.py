d05988429a4e3baa134da45ab0c6ef29bf38d5e1
Alex Soble <alex.soble@gsa.gov>
2019-12-23 10:36:34 -0800

    Add more boolean variables and test cases

--- openfisca_country_template/variables/head_start.py
+++ openfisca_country_template/variables/head_start.py
@@ -15,6 +15,22 @@ class homelessness(Variable):
     reference = "https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1305-2-terms"
 
 
+class fostercare(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+    label = "fostercare"
+    reference = "https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1305-2-terms"
+
+
+class eligible_tanf_or_ssi(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+    label = "eligible_tanf_or_ssi"
+    reference = "https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1305-2-terms"
+
+
 class eligible(Variable):
     value_type = bool
     entity = Family
@@ -23,4 +39,7 @@ class eligible(Variable):
 
     def formula(family, period):
         homelessness = family('homelessness', period)
-        return homelessness
+        fostercare = family('fostercare', period)
+        eligible_tanf_or_ssi = family('eligible_tanf_or_ssi', period)
+
+        return (homelessness or fostercare or eligible_tanf_or_ssi)
