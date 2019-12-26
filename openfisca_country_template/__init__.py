5fbcb0b6c6ee344c23f47fc44cf3c34355c95aa1
Alex Soble <alex.soble@gsa.gov>
2019-12-26 11:39:29 -0800

    Add dummy parameters to get the API up and running

--- openfisca_country_template/__init__.py
+++ openfisca_country_template/__init__.py
@@ -26,6 +26,6 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
 
         # We define which variable, parameter and simulation example will be used in the OpenAPI specification
         self.open_api_config = {
-            "variable_example": "disposable_income",
-            "parameter_example": "taxes.income_tax_rate",
+            "variable_example": "homelessness",
+            "parameter_example": "dummy_example",
             }
