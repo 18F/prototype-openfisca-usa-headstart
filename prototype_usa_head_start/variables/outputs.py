8e9e044e10351ba1a5567521adb0543f8e577703
Alex Soble <alex.soble@gsa.gov>
2019-12-30 09:53:01 -0800

    More work on vectorializing

--- prototype_usa_head_start/variables/outputs.py
+++ prototype_usa_head_start/variables/outputs.py
@@ -5,43 +5,49 @@
 
 from openfisca_core.model_api import *
 from prototype_usa_head_start.entities import *
+import numpy as np
+
+
+def household_to_cutoff_value(household_size_and_fpl_scale):
+    household_size = household_size_and_fpl_scale[0]
+    scale = household_size_and_fpl_scale[1]
+
+    if (0 < household_size < 9):
+        return scale[str(household_size) + "_person_household"]
+    else:
+        return (
+            scale["8_person_household"]
+            + ((household_size - 8) * (scale['additional_per_person_above_8']))
+            )
 
 
 class federal_poverty_line_value(Variable):
-    value_type = bool
+    value_type = int
     entity = Family
     definition_period = YEAR
     label = u"What is the federal poverty line cutoff for a family based on their state of residence and their household size?"
 
     def formula(family, period, parameters):
-        household_size = family('household_size', period)
         state_or_territory = family('state_or_territory', period)
         federal_poverty_levels = parameters(period).federal_poverty_level
         ak_poverty_levels = federal_poverty_levels['AK']
         hi_poverty_levels = federal_poverty_levels['HI']
         default_poverty_levels = federal_poverty_levels['DEFAULT']
 
-        by_state_poverty_levels = select(
+        federal_poverty_line_scale = select(
             [state_or_territory == 'AK', state_or_territory == 'HI'],
             [ak_poverty_levels, hi_poverty_levels],
             default_poverty_levels
             )
 
-        by_household_size_key = concat(household_size, '_person_household')
+        household_size = family('household_size', period)
 
-        ## TODO: Make this work
-        print('by_household_size_key')
-        print(by_household_size_key)
-        return False
+        household_size_and_fpl_scale = np.column_stack((household_size, federal_poverty_line_scale))
 
-        # if (9 > household_size):
-        #     household_size_key = str(household_size[0]) + "_person_household"
-        #     cutoff_value = local_guidelines[household_size_key]
-        # else:
-        #     eight_person_cutoff = local_guidelines["8_person_household"]
-        #     additional_per_person = local_guidelines["additional_per_person_above_8"]
-        #     people_above_8 = household_size - 8
-        #     cutoff_value = eight_person_cutoff + (additional_per_person * people_above_8)
+        return list(map(
+                    household_to_cutoff_value,
+                    household_size_and_fpl_scale
+                    ))
 
 
 class below_federal_poverty_level(Variable):
