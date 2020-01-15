6420808d3dc32a5e249635b728fa0715456c8bd3
Alex Soble <alex.soble@gsa.gov>
2020-01-15 15:47:23 -0600

    Add tests; handle and document 100% FPL and 130% FPL cases explicitly

--- prototype_usa_head_start/variables/outputs.py
+++ prototype_usa_head_start/variables/outputs.py
@@ -53,15 +53,18 @@ class federal_poverty_line_value(Variable):
                     ))
 
 
-class below_federal_poverty_level(Variable):
+class equal_to_or_below_federal_poverty_level(Variable):
     value_type = bool
     entity = Family
     definition_period = YEAR
     label = u"Is the family's income below the federal poverty level?"
+    # From the policy:
+    # "Eligibility requirements. (1) A pregnant woman or a child is eligible if: (i) The family’s income is equal to or below the poverty line...""
+    # SOURCE: https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1302-12-determining-verifying-documenting-eligibility
 
     def formula(family, period, parameters):
         return (
-            family('federal_poverty_line_value', period) > family('income', period)
+            family('federal_poverty_line_value', period) >= family('income', period)
             )
 
 
@@ -70,10 +73,14 @@ class income_between_fpl_and_130_fpl(Variable):
     entity = Family
     definition_period = YEAR
     label = u"Is the family's income below 130 percent of the federal poverty level?"
+    # From the policy:
+    # If a program chooses to enroll participants who do not meet a criterion in paragraph (c) of this section, and whose family incomes are between 100 and 130 percent of the poverty line, ...
+    # NOTE (ARS): Since the eligibility rules reference "equal to or below the poverty line" for purposes of determining eligibility, I interpret "between 100 and 130 percent" to be inclusive of 130 percent.
+    # SOURCE: https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1302-12-determining-verifying-documenting-eligibility
 
     def formula(family, period, parameters):
         return (
-            (family('federal_poverty_line_value', period) * 1.3) > family('income', period) > family('federal_poverty_line_value', period)
+            (family('federal_poverty_line_value', period) * 1.3) >= family('income', period) > family('federal_poverty_line_value', period)
             )
 
 
@@ -88,7 +95,7 @@ class head_start_eligibility_bool(Variable):
             family('homelessness', period)
             + family('fostercare', period)
             + family('eligible_tanf_or_ssi', period)
-            + family('below_federal_poverty_level', period)
+            + family('equal_to_or_below_federal_poverty_level', period)
             )
 
 
@@ -132,7 +139,7 @@ class head_start_eligibility_status(Variable):
 
         with_poverty_line_factor = add_eligibility_reason(
             with_tanf_ssi_factor,
-            family('below_federal_poverty_level', period),
+            family('equal_to_or_below_federal_poverty_level', period),
             ' Eligible because family is below the federal poverty line.'
             )
 
